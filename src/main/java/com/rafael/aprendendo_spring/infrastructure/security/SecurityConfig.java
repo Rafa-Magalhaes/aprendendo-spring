efbd4703322592dc6f3466e48c9ad82114dd0520
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-05-09 00:55:30 -0300

    fix: corrige SecurityConfig usando @RequiredArgsConstructor

--- src/main/java/com/rafael/aprendendo_spring/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/rafael/aprendendo_spring/infrastructure/security/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.rafael.aprendendo_spring.infrastructure.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
@@ -28,7 +29,6 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/usuario", "/usuario/login").permitAll()
-                        .requestMatchers("/auth").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
